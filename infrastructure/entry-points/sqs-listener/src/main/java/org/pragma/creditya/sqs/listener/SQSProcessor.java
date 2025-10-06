7488a5ee110fcf27a44919b258a7cd0e1f811288
hans.arias <hansarias74@gmail.com>
2025-10-05 19:53:04 -0500

    feat(sqs-listener) adjust listener

--- infrastructure/entry-points/sqs-listener/src/main/java/org/pragma/creditya/sqs/listener/SQSProcessor.java
+++ infrastructure/entry-points/sqs-listener/src/main/java/org/pragma/creditya/sqs/listener/SQSProcessor.java
@@ -1,6 +1,9 @@
 package org.pragma.creditya.sqs.listener;
 
 import lombok.RequiredArgsConstructor;
+import org.pragma.creditya.sqs.listener.helper.SQSSerializerHelper;
+import org.pragma.creditya.usecase.command.ResolveApplicationLoanCommand;
+import org.pragma.creditya.usecase.service.ILoanApplicationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -12,14 +15,17 @@ import java.util.function.Function;
 @Service
 @RequiredArgsConstructor
 public class SQSProcessor implements Function<Message, Mono<Void>> {
-    // private final MyUseCase myUseCase;
-
+    private final ILoanApplicationService service;
+    private final SQSSerializerHelper serializerHelper;
     private final Logger logger = LoggerFactory.getLogger(SQSProcessor.class);
 
     @Override
     public Mono<Void> apply(Message message) {
         logger.info("[infra.entrypoint.sqs-listener] (apply) payload=[ message.body:{} ]", message.body());
-        return Mono.empty();
-        // return myUseCase.doAny(message.body());
+        ResolveApplicationLoanCommand command = serializerHelper.deserialize(message.body());
+        return service.resolutionApplicationLoan(command)
+                .doOnSuccess(response -> logger.info("[infra.entrypoint.sqs-listener] (apply) success process resolution application, response=[ loan:{} ]", response))
+                .doOnError(err -> logger.info("[infra.entrypoint.sqs-listener] (apply) error process resolution application, response=[ errorMessage:{} ]", err.getMessage()))
+                .then();
     }
 }
