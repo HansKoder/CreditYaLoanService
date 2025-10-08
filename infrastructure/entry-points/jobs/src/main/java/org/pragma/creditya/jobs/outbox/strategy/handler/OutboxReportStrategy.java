d6cd914c367547d9e75716d1d40b45fe714be279
hans.arias <hansarias74@gmail.com>
2025-10-08 00:53:18 -0500

    feat(infra) set up report for scheduler job outbox message

---
+++ infrastructure/entry-points/jobs/src/main/java/org/pragma/creditya/jobs/outbox/strategy/handler/OutboxReportStrategy.java
@@ -0,0 +1,50 @@
+package org.pragma.creditya.jobs.outbox.strategy.handler;
+
+import org.pragma.creditya.jobs.outbox.helper.SQSHelper;
+import org.pragma.creditya.jobs.outbox.strategy.OutboxProcessStrategy;
+import org.pragma.creditya.model.loan.gateways.SQSProducer;
+import org.pragma.creditya.usecase.outbox.LoanOutboxMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import static org.pragma.creditya.jobs.outbox.constant.OutboxConstant.APPROVED_EVENT;
+
+@Component
+public class OutboxReportStrategy implements OutboxProcessStrategy {
+
+    @Qualifier("SQSSenderNotificationAdapter")
+    private final SQSProducer sqsProducer;
+
+    private final SQSHelper sqsHelper;
+
+    private final Logger logger = LoggerFactory.getLogger(OutboxReportStrategy.class);
+
+    public OutboxReportStrategy(
+            @Qualifier("SQSSenderReportAdapter") SQSProducer sqsProducer,
+            SQSHelper sqsHelper
+    ) {
+        this.sqsHelper = sqsHelper;
+        this.sqsProducer = sqsProducer;
+    }
+
+    @Override
+    public Mono<Boolean> apply(LoanOutboxMessage outboxMessage) {
+        logger.info("[infra.entrypoint.scheduler.report] (strategy) (apply-01) payload=[ outbox:{} ]", outboxMessage);
+        return Mono.fromCallable(() -> shouldBeSentAnyQueue(outboxMessage))
+                .doOnSuccess(response -> logger.info("[infra.entrypoint.scheduler.report] (strategy) (apply-02) response=[ isExecuted:{} ]", response));
+    }
+
+    private Boolean shouldBeSentAnyQueue (LoanOutboxMessage outboxMessage) {
+        return outboxMessage.getType().equals(APPROVED_EVENT);
+    }
+
+    @Override
+    public Mono<Void> execute(LoanOutboxMessage outbox) {
+        logger.info("[infra.entrypoint.scheduler.report] (strategy) (execute-01) payload=[ outbox:{} ]", outbox);
+        return sqsHelper.publish(sqsProducer, outbox)
+                .doOnSuccess(response -> logger.info("[infra.entrypoint.scheduler.report] (strategy) (execute-02) queue was executed with successful"));
+    }
+}
