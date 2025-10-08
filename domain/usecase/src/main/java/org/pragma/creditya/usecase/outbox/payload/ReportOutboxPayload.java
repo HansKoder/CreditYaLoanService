cf0f9b5b40a785d049fe4847a70ac9b5040f23f5
hans.arias <hansarias74@gmail.com>
2025-10-08 00:41:31 -0500

    feat(report) add outbox use case (report)

--- domain/usecase/src/main/java/org/pragma/creditya/usecase/outbox/payload/ReportOutboxPayload.java
+++ domain/usecase/src/main/java/org/pragma/creditya/usecase/outbox/payload/ReportOutboxPayload.java
@@ -1,4 +1,19 @@
 package org.pragma.creditya.usecase.outbox.payload;
 
-public class ReportOutboxPayload {
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+
+@ToString
+@Getter
+@Setter
+@Builder
+public class ReportOutboxPayload implements OutboxPayload {
+
+    private String loanId;
+    private BigDecimal amount;
+
 }
